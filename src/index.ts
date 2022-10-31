866de34da8a83c185d4bd742f6237d4d211c1418
iivanggarcia <iivanggarcia0@gmail.com>
2022-10-31 14:45:46 -0600

    Fix: imports y declaración del nuevo componente modificado

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ async function execute(options: InputOptions, context: BuilderContext): Promise<
     try {
         let { direccion, nombre, modulo } = options
         const rutaNombre = direccion + "/" + nombre + "/" + nombre;
-        const rutaModulo = ""+ modulo;
+        const rutaModulo = "" + modulo;
         console.log("Creando la carpeta...");
         const crearFolder = await fs.mkdir(direccion + "/" + nombre, () => { });
         console.log("Se ha creado el directorio con exito", crearFolder);
@@ -44,8 +44,6 @@ async function generarHTML(rutaNombre: string): Promise<any> {
 }
 
 async function generarTS(rutaNombre: string, nombre: string): Promise<any> {
-    console.log("nombre que entra", nombre);
-    console.log("ruta que entra", rutaNombre);
     let nuevoNombreComponente = nombre.charAt(0).toUpperCase() + quitarGuion(nombre.slice(1));
     let textoTs = ""
         + "import { Component, OnInit } from '@angular/core';\n"
@@ -79,35 +77,36 @@ function quitarGuion(nombre: string): string {
     let posicionGuion = nombre.indexOf("-");
     while (posicionGuion != -1) {
         //Lógica si encuentra un "-"
-        txt= txt + nombre.slice(posicionInicio,posicionGuion);
-        txt = txt + nombre.charAt(posicionGuion+1).toUpperCase();
-        posicionInicio = posicionGuion+2;
+        txt = txt + nombre.slice(posicionInicio, posicionGuion);
+        txt = txt + nombre.charAt(posicionGuion + 1).toUpperCase();
+        posicionInicio = posicionGuion + 2;
         posicionGuion = nombre.indexOf("-", posicionInicio);
     }
-    txt= txt + nombre.slice(posicionInicio,nombre.length);
+    txt = txt + nombre.slice(posicionInicio, nombre.length);
     return txt;
 }
 
 async function modificarModulo(rutaModulo: string, nombre: string): Promise<any> {
     let nuevoNombreComponente = nombre.charAt(0).toUpperCase() + quitarGuion(nombre.slice(1));
-    let str1 = "import { " + nuevoNombreComponente + "Component } from './"+ nombre +"/"+ nombre +".component';\n";
-    let str2 = "    "+ nuevoNombreComponente + "Component,";
-    let index = 0;
-    fs.readFile(rutaModulo, 'utf-8', function(err, data){
-        if (err) throw err;
-        
-        let txt = data;
-        txt = str1 + txt;
+    let str1 = "import { " + nuevoNombreComponente + "Component } from './" + nombre + "/" + nombre + ".component';";
+    let str2 = "    " + nuevoNombreComponente + "Component";
+    let txt = fs.readFileSync(rutaModulo, 'utf-8');
 
-        index = txt.indexOf("declarations: [");
-        let txt1 = txt.replace("declarations: [","declarations: [\n"+str2);
-    
-        fs.writeFile(rutaModulo, txt1, 'utf-8', function (err) {
-          if (err) throw err;
-          console.log("Escritura de archivo completa");
-        });
-      });
-      console.log('index', index);
+    //txt = str1 + txt;
+
+    let index1 = txt.lastIndexOf("import {");
+    let index2 = txt.lastIndexOf(";");
+    txt = txt.replace(txt.slice(index1,index2+1),txt.slice(index1,index2+1)+"\n"+str1);
+
+    index1 = txt.indexOf("declarations: [");
+    index2 = txt.indexOf("],", index1);
+    txt = [txt.slice(0,index2-3)+",",str2,"  "+txt.slice(index2,txt.length)].join("\n");
+    //txt = txt.replace("declarations: [", "declarations: [\n" + str2);
+
+    fs.writeFile(rutaModulo, txt, 'utf-8', function (err) {
+        if (err) throw err;
+        console.log("Escritura de archivo completa");
+    });
 }
 
 export default createBuilder(execute);
